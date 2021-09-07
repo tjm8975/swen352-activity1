c269dc328593ae82cbefb7fbcd3673b93395b057
tjm8975 <tjm8975@g.rit.edu>
2021-09-07 18:13:55 -0400

    Initial setup for InventoryTest

--- unittests/coffeemaker/InventoryTest.java
+++ unittests/coffeemaker/InventoryTest.java
@@ -5,15 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import junit.framework.TestCase;
 
-public class InventoryTest {
+public class InventoryTest extends TestCase {
+	private Inventory inv;
+	private Recipe rec;
 
 	@Before
 	public void setUp() throws Exception {
+		inv = new Inventory();
+		rec = new Recipe();
+		super.setUp();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		inv = null;
+		rec = null;
+		super.tearDown();
 	}
 
 	@Test
