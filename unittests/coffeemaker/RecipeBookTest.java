0b552525d2617839845bfb8eb58ee44cbe9be95f
Jacob Auger <jga9325@rit.edu>
2021-09-09 17:58:49 -0400

    Added first 2 tests

--- unittests/coffeemaker/RecipeBookTest.java
+++ unittests/coffeemaker/RecipeBookTest.java
@@ -5,19 +5,33 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import junit.framework.TestCase;
 
-public class RecipeBookTest {
+public class RecipeBookTest extends TestCase {
+	private RecipeBook rb;
+	private Recipe recipe;
 
 	@Before
 	public void setUp() throws Exception {
+		rb = new RecipeBook();
+		recipe = new Recipe();
+		super.setUp();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		rb = null;
+		recipe = null;
+		super.tearDown();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetRecipe() {
+		assertEquals(rb.getRecipes().length, 4);
+	}
+	
+	@Test
+	public void testAddRecipeReturn() {
+			assertEquals(rb.addRecipe(recipe), true);
 	}
 }
